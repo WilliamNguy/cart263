5daa0ba08b6b57f93d6e72837affe2bfc6c7130e
WilliamNguy <144380539+WilliamNguy@users.noreply.github.com>
2024-03-07 16:03:26 -0500

    testing one of the examples

---
+++ exercises/Phaser 3 test/js/script.js
@@ -0,0 +1,16 @@
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: true
+        }
+    },
+    scene: [Example]
+};
+
+const game = new Phaser.Game(config);
